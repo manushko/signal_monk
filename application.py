855e92848743d84b37d43ef53c81a9c70587943e
Vladimir Manushko <manushko.vladimir@steelkiwi.com>
2020-05-24 17:30:18 +0800

    Add MongoDB as primary database storage

--- application.py
+++ application.py
@@ -1,9 +1,17 @@
+import os
 import ssl
 import feedparser
 from flask import Flask, render_template
-
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://{username}:{password}@db:27017/{db}".format(
+    username=os.environ['MONGODB_USERNAME'],
+    password=os.environ['MONGODB_PASSWORD'],
+    db=os.environ['MONGO_INITDB_DATABASE']
+)
+
+mongo = PyMongo(app)
 
 
 def get_feeds():
@@ -18,6 +26,7 @@ def get_feeds():
 
 @app.route('/')
 def index():
+    mongo.db.feeds.find_one()
     if hasattr(ssl, '_create_unverified_context'):
         ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -25,13 +34,5 @@ def index():
 
     data = [feedparser.parse(feed) for feed in feeds]
 
-    #for feed in feeds:
-    #    d = feedparser.parse(feed)
-    #    for i in range(10):
-    #        entry = f'{d.entries[i].title} {d.entries[i].published} {d.entries[i].link}'
-    #        response += f'/n{entry}'
-    
-    
- 
     return render_template('index.html', feeds=data)
 
